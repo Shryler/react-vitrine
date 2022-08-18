a087127c02f895bd833a29246aee9b62cb0640e3
Shryler <105290336+Shryler@users.noreply.github.com>
2022-08-18 18:17:18 +0200

    add nav

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+import Project1 from './pages/Project1';
+import Project2 from './pages/Project2';
+import Project3 from './pages/Project3';
+import Project4 from './pages/Project4';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route></Route>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/projet-1' element={<Project1></Project1>}></Route>
+        <Route path='/projet-2' element={<Project2></Project2>}></Route>
+        <Route path='/projet-3' element={<Project3></Project3>}></Route>
+        <Route path='/projet-4' element={<Project4></Project4>}></Route>
+        <Route path='/contact' element={<Contact></Contact>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
       </Routes>
     </BrowserRouter>
   );
